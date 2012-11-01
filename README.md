5753d56dce4ba09ece0f398983cb7370e474dc2a
Unia <jthidskes@live.nl>
2012-11-01 11:04:26 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+This RuneScape client is put together by HikariKnight
+to create a completely crossplatform RuneScape client that can be run
+without installation on any platform, even platforms not supported by Jagex. 
+
+I (Unia) just slimmed it down to match only my setup; Linux without Wine and no PulseAudio.
+More features might be removed in the future.
+
+The client files are from the Windows client for RuneScape.
+
+The "RuneScape" perl script is written by HikariKnight, but is based on
+Ethoxyethaan's shellscript to run the RuneScape Windows client natively.
+
+For more information, see HikariKnight's website:
+http://hkprojects.weebly.com
+
+RuneScape is a registred trademark of Jagex Inc.
+
---
+++ modules/rsu_check_for_jagexappletviewer.pm
@@ -0,0 +1,30 @@
+package rsu_cfjav;
+
+	sub check_for_jagexappletviewer
+	{
+		# Get the rsu_data container
+		my $rsu_data = shift;
+		
+		# Pass the data to some variables that can be used in commands
+		my $cwd = $rsu_data->cwd;
+		my $clientdir = $rsu_data->clientdir;
+		
+		my $jarfileexistcheck = "";
+
+		# Execute ls and pipe the result to grep to check if jagexappletviewer.jar exists
+		$jarfileexistcheck = `ls \"$clientdir/bin/\" | grep \"jagexappletviewer.jar\"`;
+
+		# Transfer the result to a new variable
+		my $jarcheckresult = $jarfileexistcheck;
+
+		# If jagexappletviewer.jar does not exist then
+		if ($jarcheckresult !~ /jagexappletviewer.jar/)
+		{
+			# Make a variable containing the path to the update script
+			my $updatescript = "\"$cwd/update-runescape-client\"";
+			
+			# Execute the updater
+			system "$updatescript";
+		}
+	}
+1;
