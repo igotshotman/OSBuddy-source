e817d52b394f85bb87f8de38db11e4cca36c6a3f
Unia <jthidskes@live.nl>
2013-02-02 21:41:50 +0100

    moved to glib instead of standard C, moved About button to bottom

--- runescape-launcher.c
+++ runescape-launcher.c
@@ -17,40 +17,41 @@
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/*#include <config.h>*/
 #include <gtk/gtk.h>
 #include <webkit/webkit.h>
+#include <glib.h>
+#include <glib/gstdio.h>
+#include <glib/gprintf.h>
 #include <glib/gi18n.h>
 #include <unistd.h>
 
-char buf[FILENAME_MAX], *str;
-ssize_t len;
+gchar *installpath;
 
 char *
 getcurrentpath()
 {
-	if((len=readlink("/proc/self/exe", buf, sizeof(buf) -1))!=-1){
-		buf[len]=0;
-		if((str=strrchr(buf,'/')))
-			str[1]=0;
-	}
-	return buf;
+	installpath = g_get_current_dir();
+	return installpath;
 }
 
 static void
 launch_client (GtkButton* button)
 {
+	gchar *runescape_launch;
+
 	getcurrentpath();
-	strcat(buf, "runescape");
-	execl(buf, buf, NULL);
+	runescape_launch = g_build_filename(installpath, "runescape", NULL);
+	execl(runescape_launch, runescape_launch, NULL);
 }
 
 static void
 update_client (GtkButton* button)
 {
+	gchar *runescape_update;
+
 	getcurrentpath();
-	strcat(buf, "runescape-update-client");
-	execl(buf, buf, NULL);
+	runescape_update = g_build_filename(installpath, "runescape-update-client", NULL);
+	execl(runescape_update, runescape_update, NULL);
 }
 
 static void
@@ -66,7 +67,6 @@ dialog_open (GSimpleAction *simple, GVariant *parameter)
 
 	about_dialog = gtk_about_dialog_new ();
 
-	/* Information we are going to send to the About dialog */
 	const gchar *authors[] = {"Unia (Jente)", "HikariKnight", NULL};
 	const gchar *license[] = {
 		N_("The RuneScape Client Launcher is free software: you can redistribute it and/or modify "
@@ -81,11 +81,9 @@ dialog_open (GSimpleAction *simple, GVariant *parameter)
 		   "along with this program. If not, see <http://www.gnu.org/licenses/>.")
 };
 
-	/* Paste the license together */
 	gchar *license_trans;
 	license_trans = g_strjoin ("\n\n", _(license[0]), _(license[1]), _(license[2]), NULL);
 
-	/* We fill in the information for the about dialog */
 	gtk_about_dialog_set_program_name (GTK_ABOUT_DIALOG (about_dialog), "RuneScape Client for Linux");
 	gtk_about_dialog_set_comments (GTK_ABOUT_DIALOG (about_dialog), "Play RuneScape on Linux"),
 	gtk_about_dialog_set_copyright (GTK_ABOUT_DIALOG (about_dialog), "Copyright \xc2\xa9 2013 Unia (Jente)");
@@ -96,10 +94,8 @@ dialog_open (GSimpleAction *simple, GVariant *parameter)
 	gtk_about_dialog_set_website (GTK_ABOUT_DIALOG (about_dialog), "https://github.com/Unia/runescape-client");
 	gtk_about_dialog_set_logo_icon_name (GTK_ABOUT_DIALOG (about_dialog), "runescape");
 
-	/* Close About dialog when close is clicked */
 	g_signal_connect (GTK_DIALOG (about_dialog), "response", G_CALLBACK (dialog_close), NULL);
 
-	/* Show the About dialog */
 	gtk_widget_show (about_dialog);
 }
 
@@ -129,7 +125,7 @@ create_window (void)
 	/* Add all buttons to box_button */
 	gtk_box_pack_start (GTK_BOX (box_button), button_play, FALSE, FALSE, 0);
 	gtk_box_pack_start (GTK_BOX (box_button), button_update, FALSE, FALSE, 0);
-	gtk_box_pack_start (GTK_BOX (box_button), button_about, FALSE, FALSE, 0);
+	gtk_box_pack_end (GTK_BOX (box_button), button_about, FALSE, FALSE, 0);
 
 	/* Create a box to hold the scroll container */
 	box_scroll = gtk_box_new (GTK_ORIENTATION_VERTICAL, 5);
@@ -168,18 +164,9 @@ main (int argc, char *argv[])
 {
  	GtkWidget *window;
 
-#ifdef ENABLE_NLS
-	bindtextdomain (GETTEXT_PACKAGE, PACKAGE_LOCALE_DIR);
-	bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
-	textdomain (GETTEXT_PACKAGE);
-#endif
-
 	gtk_init (&argc, &argv);
-
 	window = create_window ();
 	gtk_widget_show_all (window);
-
 	gtk_main ();
-
 	return 0;
 }
