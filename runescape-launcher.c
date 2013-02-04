493fcc232292e6f3e6a2db6bedeeed1e7ad7e686
Unia <jthidskes@live.nl>
2013-02-04 14:17:21 +0100

    removed pedantic compile warnings, cleaned up webkit code in launcher

--- runescape-launcher.c
+++ runescape-launcher.c
@@ -66,15 +66,13 @@ dialog_open ()
 {
 	GtkWidget *about_dialog;
 
-	about_dialog = gtk_about_dialog_new ();
-
 	const gchar *authors[] = {"Unia (Jente)", "HikariKnight", NULL};
 	const gchar *license[] = {
-		N_("The RuneScape Client Launcher is free software: you can redistribute it and/or modify "
+		N_("The RuneScape Client is free software: you can redistribute it and/or modify "
 		   "it under the terms of the GNU General Public License as published by "
 		   "the Free Software Foundation, either version 3 of the License, or "
 		   "(at your option) any later version."),
-		N_("The RuneScape Launcher is distributed in the hope that it will be useful "
+		N_("The RuneScape Client is distributed in the hope that it will be useful "
 		   "but WITHOUT ANY WARRANTY; without even the implied warranty of "
 		   "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the "
 		   "GNU General Public License for more details."),
@@ -85,6 +83,7 @@ dialog_open ()
 	gchar *license_trans;
 	license_trans = g_strjoin ("\n\n", _(license[0]), _(license[1]), _(license[2]), NULL);
 
+	about_dialog = gtk_about_dialog_new ();
 	gtk_about_dialog_set_program_name (GTK_ABOUT_DIALOG (about_dialog), "RuneScape Client for Linux");
 	gtk_about_dialog_set_comments (GTK_ABOUT_DIALOG (about_dialog), "Play RuneScape on Linux"),
 	gtk_about_dialog_set_copyright (GTK_ABOUT_DIALOG (about_dialog), "Copyright \xc2\xa9 2013 Unia (Jente)");
@@ -104,14 +103,16 @@ static GtkWidget*
 create_window (void)
 {
 	GtkWidget *window, *box_all, *box_scroll, *box_button, *button_play, *button_update, *button_about, *scroll;
+	GtkWidget *view;
 
 	/* Set up the UI */
 	window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
 	gtk_window_set_default_size (GTK_WINDOW (window), 800, 600);
 	gtk_window_set_title (GTK_WINDOW (window), "RuneScape Launcher");
 	gtk_window_set_default_icon_name ("runescape");
+	g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
 
-	/* Create box container to hold the webkit view and both buttons */
+	/* Create a box to hold the webkit view and box_buttons */
 	box_all = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 5);
 	gtk_container_set_border_width (GTK_CONTAINER (box_all), 5);
 
@@ -127,6 +128,12 @@ create_window (void)
 	gtk_box_pack_start (GTK_BOX (box_button), button_play, FALSE, FALSE, 0);
 	gtk_box_pack_start (GTK_BOX (box_button), button_update, FALSE, FALSE, 0);
 	gtk_box_pack_end (GTK_BOX (box_button), button_about, FALSE, FALSE, 0);
+	/* Launch RuneScape client when clicking button_play */
+	g_signal_connect (button_play, "clicked", G_CALLBACK (launch_client), NULL);
+	/* Update the client when clicking button_update */
+	g_signal_connect (button_update, "clicked", G_CALLBACK (update_client), NULL);
+	/* Open About dialog when clicking button_about */
+	g_signal_connect (button_about, "clicked", G_CALLBACK (dialog_open), NULL);
 
 	/* Create a box to hold the scroll container */
 	box_scroll = gtk_box_new (GTK_ORIENTATION_VERTICAL, 5);
@@ -134,8 +141,8 @@ create_window (void)
 	scroll = gtk_scrolled_window_new (NULL, NULL);
 	g_object_set (scroll, "shadow-type", GTK_SHADOW_IN, NULL);
 	/* Create webkit view and point it to runescape.com */
-	WebKitWebView *view = WEBKIT_WEB_VIEW(webkit_web_view_new());
-	webkit_web_view_load_uri(view, "http://services.runescape.com/m=news/list.ws");
+	view = webkit_web_view_new();
+	webkit_web_view_load_uri((WebKitWebView *)view, "http://services.runescape.com/m=news/list.ws");
 	/* Add VIEW to SCROLL */
 	gtk_container_add (GTK_CONTAINER (scroll), GTK_WIDGET(view));
 	/* Add scroll to box_scroll */
@@ -147,15 +154,6 @@ create_window (void)
 	gtk_box_pack_start (GTK_BOX (box_all), box_button, FALSE, FALSE, 0);
 	/* Add box_all to TOPLEVEL WINDOW */
 	gtk_container_add (GTK_CONTAINER (window), box_all);
-
-	/* Launch RuneScape client when clicking button_play */
-	g_signal_connect (button_play, "clicked", G_CALLBACK (launch_client), NULL);
-	/* Update the client when clicking button_update */
-	g_signal_connect (button_update, "clicked", G_CALLBACK (update_client), NULL);
-	/* Open About dialog when clicking button_about */
-	g_signal_connect (button_about, "clicked", G_CALLBACK (dialog_open), NULL);
-	/* Exit when the window is closed */
-	g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
 
 	return window;
 }
